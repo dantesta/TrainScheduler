36d718dc32e0eaae9fc0b72cf131a1d068fda33d
Dan Testa <dantesta@Dans-MacBook-Pro-3.local>
2017-09-16 04:57:15 -0400

    prints to DOM and firebase, still needs time conversion

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 
   // Initialize Firebase
   var config = {
@@ -8,6 +10,70 @@
     storageBucket: "trainscheduler-c89f2.appspot.com",
     messagingSenderId: "151663074715"
   };
+    firebase.initializeApp(config);
 
 
 var database = firebase.database();
+
+// 2. Button for adding Employees
+$("#submitBtn").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs user input
+  var trainName = $("#train-name-input").val().trim();
+  var trainDest = $("#dest-input").val().trim();
+  var trainFirst = moment($("#first-input").val().trim(), "DD/MM/YY").format("X");
+  var trainFreq = $("#freq-input").val().trim();
+  console.log(trainFirst)
+  // Creates local "temporary" object for holding employee data
+  var newTrain = {
+    name: trainName,
+    dest: trainDest,
+    first: trainFirst,
+    freq: trainFreq
+  };
+
+  // Uploads employee data to the database
+  database.ref().push(newTrain);
+
+
+  // Alert
+  alert("Train Schedule successfully added!");
+
+  // Clears all of the text-boxes
+  $("#train-name-input").val("");
+  $("#dest-input").val("");
+  $("#first-input").val("");
+  $("#freq-input").val("");
+});
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var trainDest = childSnapshot.val().role;
+  var trainFirst = childSnapshot.val().start;
+  var trainFreq = childSnapshot.val().rate;
+
+
+
+  var trainStartMoment = moment.unix(trainFirst).format("MM/DD/YY");
+
+  var trainSchedule = moment().diff(moment.unix(trainFirst, "X"), "months");
+
+
+  var trainTime = trainSchedule * trainFreq;
+
+
+  // Add each train's data into the table
+  $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" +
+  trainStartMoment + "</td><td>" + trainSchedule + "</td><td>" + trainFreq + "</td><td>" + trainTime + "</td></tr>");
+});
+
+
+
+});
+
