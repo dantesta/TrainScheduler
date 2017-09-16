8ff04e99de36dd15ef59a132ba911c234e16e4c9
Dan Testa <dantesta@Dans-MacBook-Pro-3.local>
2017-09-15 22:16:46 -0400

    Firebase Working

---
+++ assets/javascript/train.js
@@ -0,0 +1,13 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCFsf6RaeXD2-tMqpuVrdTSl0d8Wp4glxk",
+    authDomain: "trainscheduler-c89f2.firebaseapp.com",
+    databaseURL: "https://trainscheduler-c89f2.firebaseio.com",
+    projectId: "trainscheduler-c89f2",
+    storageBucket: "trainscheduler-c89f2.appspot.com",
+    messagingSenderId: "151663074715"
+  };
+
+
+var database = firebase.database();
